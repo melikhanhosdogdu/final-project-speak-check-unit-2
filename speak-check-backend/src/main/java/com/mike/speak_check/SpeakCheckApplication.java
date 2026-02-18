9d2c39d708268152f109358ac183565aaed7d2f1
Melikhan Hoşdoğdu <melikhan.hosdogdu@gmail.com>
2026-02-17 23:47:31 -0600

    Project set up

---
+++ speak-check-backend/src/main/java/com/mike/speak_check/SpeakCheckApplication.java
@@ -0,0 +1,13 @@
+package com.mike.speak_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakCheckApplication.class, args);
+	}
+
+}
---
+++ speak-check-backend/src/test/java/com/mike/speak_check/SpeakCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.speak_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
