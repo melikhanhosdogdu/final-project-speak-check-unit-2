830a2e794c70ca4064d094ac411e93066353f6b4
Melikhan Hoşdoğdu <melikhan.hosdogdu@gmail.com>
2026-02-18 02:21:17 -0600

    Updated SecurityConfig.java

--- speak-check-backend/src/main/java/com/mike/speak_check/config/SecurityConfig.java
+++ speak-check-backend/src/main/java/com/mike/speak_check/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.mike.speak_check.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +13,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/post/**").permitAll()
                         .anyRequest().authenticated()
