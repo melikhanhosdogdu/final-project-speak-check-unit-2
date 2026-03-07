11b4b5cbfc6bf4d99ff06ba383a448fe7ce25625
Melikhan Hoşdoğdu <melikhan.hosdogdu@gmail.com>
2026-03-07 17:23:30 -0600

    Create CommentRepository

--- speak-check-backend/src/main/java/com/mike/speak_check/repository/CommentRepository.java
+++ speak-check-backend/src/main/java/com/mike/speak_check/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.mike.speak_check.repository;
 
 import com.mike.speak_check.model.Comment;
+import com.mike.speak_check.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+    List<Comment> findByPost(Post post);
+
 }
